3ba1aa93f490a52afb490e123457d4fdee930996
areskill <areskul@areskul.com>
2025-10-17 10:16:13 +0200

    refactor(bar): workspace indicator to bottom

--- security/default.nix
+++ security/default.nix
@@ -5,27 +5,6 @@
   inputs,
   ...
 }: {
-  # Loosen Security for fast sudoing
-  security.sudo.extraRules = [
-    {
-      groups = ["wheel"];
-      commands = [
-        {
-          command = "ALL";
-          options = ["NOPASSWD"];
-        }
-      ];
-    }
-  ];
-
-  users.groups = {
-    wheel.members = config.normal.users;
-  };
-
-  services.dbus.implementation = "broker";
-
-  security.polkit.enable = true;
-
   environment.systemPackages = with pkgs; [
     #doas
 
