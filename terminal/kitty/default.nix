e7352e7eda38067515d1ecf7f2135eb40d666333
areskill <areskul@areskul.com>
2025-10-29 10:40:57 +0100

    fix(kitty): bad link

--- terminal/kitty/default.nix
+++ terminal/kitty/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
   systemd.user.tmpfiles.rules = [
-    "L+ %h/.config/kitty/current-theme.conf - - - - %h/.config/kitty/doom_chad.conf"
+    "L+ %h/.config/kitty/current-theme.conf - - - - %h/.config/kitty/themes/doom_chad.conf"
   ];
 }
